60915b25818f75997df8729bd89885d08fbfe3e5
snnbyyds <snnbyyds@gmail.com>
2023-10-04 16:41:43 +0800

    lahaina: Exclude GKI folders when loading vendor modules https://gerrit.twrp.me/c/android_bootable_recovery/+/6620
    Signed-off-by: snnbyyds <snnbyyds@gmail.com>
    Signed-off-by: mujianwu <lrh15563910083@163.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -218,6 +218,7 @@ TW_HAS_EDL_MODE := true
 TW_INCLUDE_NTFS_3G := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko snd_event_dlkm.ko msm_drm.ko q6_dlkm.ko q6_notifier_dlkm.ko q6_pdr_dlkm.ko"
+TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
 TW_MAX_BRIGHTNESS := 225
 TW_STATUS_ICONS_ALIGN := center
 TWRP_INCLUDE_LOGCAT := true
